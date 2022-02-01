f04f29b268b184617b837d2067ee2911f4522582
RUI HU <hr00wz@gmail.com>
2022-02-01 16:22:47 -0330

    class

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+import turtle
+
+
 class Address:
     def __init__(self, street,city,state,postal_code,country):
         self.street = street
@@ -8,6 +11,7 @@ class Address:
 my_address = Address(street="TerraNova",city="St. John's", state="NL",postal_code="A1B1G1",country= "CA")
 print(my_address.city)
 import math
+from  turtle import *
 class Circle:
     def __init__(self, radius):
         self.radius = radius
@@ -15,7 +19,13 @@ class Circle:
         return math.pi*self.radius**2
     def perimeter(self):
         return 2*self.radius*math.pi
-test = Circle(1)
+    def draw(self):
+        gary = turtle.Turtle()
+        gary.pensize(5)
+        gary.color("red")
+        gary.circle(self.radius)
+        turtle.done
+test = Circle(10)
 print(test.area() )
 print(test.perimeter())
-
+print(test.draw())
