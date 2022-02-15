7eef95eeec0cbad8cce84a51dfeaa6b7fb2aacb7
RUI HU <hr00wz@gmail.com>
2022-02-15 15:02:50 -0330

    not done yet

---
+++ person and pet.py
@@ -0,0 +1,29 @@
+class Dog:
+    def __init__(self, dog_name: str, dage: int, tricks: []):
+        self.name = dog_name
+        self.age = dage
+        self.tricks = tricks
+
+    def bark(self):
+        pass
+
+    def sit(self):
+        pass
+
+
+class Person:
+    def __init__(self, pname: str, person_age: int, pet: Dog):
+        self.name = pname
+        self.age = person_age
+        self.dog = pet
+
+    def walk_dog(self):
+        pass
+
+    def train_dog(self):
+        pass
+
+
+dog = Dog(dog_name="Akira", dage=4, tricks="hunting bird")
+person_one = Person(pname="R.H", person_age=25, pet=dog)
+print(person_one.dog)
