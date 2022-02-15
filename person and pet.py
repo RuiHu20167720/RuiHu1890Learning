da396ccd5829b5cd18fccccebd2f9f7326bf3a9f
RUI HU <hr00wz@gmail.com>
2022-02-15 16:00:00 -0330

    maybe I did it right

--- person and pet.py
+++ person and pet.py
@@ -1,5 +1,5 @@
 class Dog:
-    def __init__(self, dog_name: str, dage: int, tricks: []):
+    def __init__(self, dog_name: str, dage: int, tricks: list = []):
         self.name = dog_name
         self.age = dage
         self.tricks = tricks
@@ -29,6 +29,10 @@ class Dog:
               "░▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒░▒░▒░▒▒▄▒▒▐\n"
               "░░▀▄▒▒▒▒▒▒▒▒▒▒▒░▒░▒░▒▄▒▒▒▒▌")
 
+    def train_dog(self, new_trick):
+        print(f"I try to teach {self.name} {new_trick},\n"
+              f"{self.name}: 'you old dog always have new tricks'")
+        self.tricks.append(new_trick)
 
 class Person:
     def __init__(self, pname: str, person_age: int, pet: Dog):
@@ -39,14 +43,13 @@ class Person:
     def walk_dog(self):
         print("The dog looks at me, says no")
 
-    def train_dog(self):
-        print("I try to teach the dog some tricks,\n"
-              "Dog: 'you old dog always have new tricks'")
 
-
-dog = Dog(dog_name="Akira", dage=4, tricks="hunting bird")
+dog = Dog(dog_name="Akira", dage=4)
 person_one = Person(pname="R.H", person_age=25, pet=dog)
-person_one.dog.sit()
-person_one.dog.bark()
-person_one.train_dog()
+#person_one.dog.sit()
+#person_one.dog.bark()
+new_tricks = input("Please enter the trick you want to teach: ")
+person_one.dog.train_dog(new_trick=new_tricks)
+print(f"I want to walk my dog")
 person_one.walk_dog()
+print(person_one.dog.tricks)
